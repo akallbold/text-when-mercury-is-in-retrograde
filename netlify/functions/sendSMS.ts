91add3faf45a1f5fc99961122f8d755a3b82e7d4
Anna <a.kallenbornbolden@gmail.com>
2023-11-24 18:03:34 +0100

    SMS endpoint doesnt do fetch to giphy

--- netlify/functions/sendSMS.ts
+++ netlify/functions/sendSMS.ts
@@ -1,9 +1,18 @@
 import twilio from "twilio";
 
 export const handler = async () => {
+  const today = new Date();
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
   console.log("Welcome!");
   try {
-    checkForMercuryAndSendSMS();
+    const isMercuryInRetrograde = await checkIfRetrograde(formatDate(today));
+    const wasMercuryInRetrogradeYesterday = await checkIfRetrograde(
+      formatDate(yesterday)
+    );
+    // if (isMercuryInRetrograde && !wasMercuryInRetrogradeYesterday) {
+    sendMMS();
+    // }
     return {
       statusCode: 200,
     };
@@ -15,109 +24,48 @@ export const handler = async () => {
   }
 };
 
-const checkForMercuryAndSendSMS = async () => {
+const checkIfRetrograde = async (date) => {
   const mercury_api = process.env.MERCURY_API;
-  const giphy_api = process.env.GIPHY_API;
-  const giphy_api_key = process.env.GIPHY_API_KEY;
-  if (mercury_api && giphy_api && giphy_api_key) {
-    console.log("We have a mercury_api && giphy_api && giphy_api_key!");
-    const isMercuryInRetrogradeFunctionToday = async () => {
-      console.log(
-        "Welcome to the isMercuryInRetrogradeFunctionToday function!"
-      );
-
-      try {
-        console.log("here1");
-        const response = await fetch(mercury_api, {
-          method: "GET",
-        });
-        console.log("here2");
-
-        const data = await response.json();
-        console.log("Is mercury in retrograde today?", data.is_retrograde);
-        return data.is_retrograde;
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    const wasMercuryInRetrogradeYesterdayFunction = async () => {
-      console.log(
-        "Welcome to the wasMercuryInRetrogradeYesterdayFunction function!"
-      );
-
-      const dateNow = new Date();
-      const yesterday = new Date(dateNow.setDate(dateNow.getDate() - 1));
-      const yyyy = yesterday.getFullYear();
-      const mm = String(yesterday.getMonth() + 1).padStart(2, "0");
-      const dd = String(yesterday.getDate()).padStart(2, "0");
-      const yesterdayFormatted = `${yyyy}-${mm}-${dd}`;
-      try {
-        const response = await fetch(
-          `${mercury_api}?date=${yesterdayFormatted}`,
-          {
-            method: "GET",
-          }
-        );
-        const data = await response.json();
-        console.log("Was mercury in retrograde yesterday?", data.is_retrograde);
-        return data.is_retrograde;
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    // get mercury in retrograde gif
-    const getRandomGif = async () => {
-      console.log("In getRandomGif func");
-      const searchString = "mercury+is+in+retrograde";
-
-      try {
-        const response = await fetch(
-          `${giphy_api}?api_key=${giphy_api_key}&q=${searchString}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`,
-          {
-            method: "GET",
-          }
-        );
-
-        const data = await response.json();
-        const gifArray = data.data;
-        const randomIndex = Math.floor(Math.random() * gifArray.length);
-        const randomImage = gifArray[randomIndex].images.original.url;
-        return randomImage;
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    const isMercuryInRetrograde = await isMercuryInRetrogradeFunctionToday();
-    const wasMercuryInRetrogradeYesterday =
-      await wasMercuryInRetrogradeYesterdayFunction();
-
-    // if (isMercuryInRetrograde && !wasMercuryInRetrogradeYesterday){
-    if (true) {
-      console.log(
-        "Now we are in the part of the code where we have determined we need to send the MMS"
-      );
-
-      const account_sid = process.env.ACCOUNT_SID;
-      const auth_token = process.env.AUTH_TOKEN;
-      const to_phone_number = process.env.TO_PHONE_NUMBER;
-      const from_phone_number = process.env.FROM_PHONE_NUMBER;
-      const message = `Mercury is in retrograde. Be careful out there!`;
-      const randomGif = await getRandomGif();
-      if (to_phone_number && from_phone_number && account_sid && auth_token) {
-        console.log("We have the vars we need");
-        let client = twilio(account_sid, auth_token);
-        console.log("Twilio config created");
-        client.messages
-          .create({
-            body: message,
-            to: to_phone_number,
-            from: from_phone_number,
-            mediaUrl: randomGif,
-          })
-          .then((message) => console.log(message.sid));
-      }
+  if (mercury_api) {
+    try {
+      const response = await fetch(`${mercury_api}?date=${date}`, {
+        method: "GET",
+      });
+      const data = await response.json();
+      console.log("Was mercury in retrograde on:", date, data.is_retrograde);
+      return data.is_retrograde;
+    } catch (error) {
+      console.error(error);
     }
+  }
+};
+
+const formatDate = (rawDate) => {
+  const yyyy = rawDate.getFullYear();
+  const mm = String(rawDate.getMonth() + 1).padStart(2, "0");
+  const dd = String(rawDate.getDate()).padStart(2, "0");
+  return `${yyyy}-${mm}-${dd}`;
+};
+
+const sendMMS = async () => {
+  console.log("send the SMS");
+  const account_sid = process.env.ACCOUNT_SID;
+  const auth_token = process.env.AUTH_TOKEN;
+  const to_phone_number = process.env.TO_PHONE_NUMBER;
+  const from_phone_number = process.env.FROM_PHONE_NUMBER;
+  const message = `Mercury is in retrograde. Be careful out there!`;
+  if (to_phone_number && from_phone_number && account_sid && auth_token) {
+    console.log(
+      "We have to_phone_number && from_phone_number && account_sid && auth_token"
+    );
+    let client = twilio(account_sid, auth_token);
+    console.log("Twilio config created");
+    client.messages
+      .create({
+        body: message,
+        to: to_phone_number,
+        from: from_phone_number,
+      })
+      .then((message) => console.log(message.sid));
   }
 };
