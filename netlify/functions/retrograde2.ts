ea4f384240c47da501508a1c27723d1b49de16dc
Anna <a.kallenbornbolden@gmail.com>
2023-11-25 17:22:02 +0100

    zdded gif back

--- netlify/functions/retrograde2.ts
+++ netlify/functions/retrograde2.ts
@@ -47,18 +47,17 @@ const sendSMS = async () => {
   const auth_token = process.env.AUTH_TOKEN;
   const to_phone_number = process.env.TO_PHONE_NUMBER;
   const from_phone_number = process.env.FROM_PHONE_NUMBER;
-  const message = `Mercury is in retrograde. Be careful out there!MMS`;
-  // const randomGif = await getRandomGif();
+  const message = `Mercury is in retrograde. Be careful out there! SMS`;
+  const randomGif = await getRandomGif();
   if (to_phone_number && from_phone_number && account_sid && auth_token) {
     const client = twilio(account_sid, auth_token);
-    console.log("Twilio config created", { client });
     try {
       console.log("Client.messages", client.messages);
       const resp = await client.messages.create({
         body: message,
         to: to_phone_number,
         from: from_phone_number,
-        // mediaUrl: randomGif,
+        mediaUrl: randomGif,
       });
       console.log({ resp });
       console.log("Message sent: ", resp.sid);
